911f6b2f69fcbf3a56bd325b02caeb945689e667
JooKFran <jeerawut.jd40@gmail.com>
2024-07-01 15:07:05 +0700

    handler external image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        domains: ['profile.line-scdn.net'],
+    },
+};
 
 export default nextConfig;
