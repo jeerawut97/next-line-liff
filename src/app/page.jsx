9df2329127be1d571cc6680e6dcaa6ba7d5e5270
JooKFran <jeerawut.jd40@gmail.com>
2024-07-01 14:46:53 +0700

    handler sequnce useEffect & useState

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,49 +5,22 @@ import liff from '@line/liff';
 
 const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
 
-const initialLiff = async () => {
-  try {
-    await liff.init({
-      liffId: liffId,
-      withLoginOnExternalBrowser: true,
-    });
-
-    // console.log(liff.getLanguage());
-    // console.log(liff.getVersion());
-    // console.log(liff.isInClient());
-    // console.log(liff.isLoggedIn());
-    // console.log(liff.getOS());
-    // console.log(liff.getLineVersion());
-
-    await liff.init({
-      liffId: liffId, // Use own liffId
-    })
-    .then(() => {
-      liff.getDecodedIDToken();
-      // const token = liff.getDecodedIDToken();
-      // console.log(token); // print decoded token object
-
-      // const getAccessToken = liff.getAccessToken()
-      // console.log(`getAccessToken: ${getAccessToken}`)
-
-      // const profile = liff.getProfile()
-      // console.log(`profile: ${profile}`)
-
-      // const profilePlus = liff.getProfilePlus()
-      // console.log(`profilePlus: ${profilePlus}`)
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
-
 export default function Home() {
   const [profile, setProfile] = useState(null)
   console.log(`before profile: ${profile}`)
 
-  useEffect(() => {
+  useEffect( async () => {
     if (typeof window !== 'undefined') {
-      setProfile(initialLiff())
+      try {
+        await liff.init({
+          liffId: liffId,
+          withLoginOnExternalBrowser: true,
+        }).then(() => {
+          setProfile(liff.getDecodedIDToken());
+        });
+      } catch (err) {
+        console.error(err);
+      }
     } else {
       console.log('window undefined')
     }
@@ -71,3 +44,23 @@ export default function Home() {
     </main>
   );
 }
+
+
+// console.log(liff.getLanguage());
+// console.log(liff.getVersion());
+// console.log(liff.isInClient());
+// console.log(liff.isLoggedIn());
+// console.log(liff.getOS());
+// console.log(liff.getLineVersion());
+
+// const token = liff.getDecodedIDToken();
+// console.log(token); // print decoded token object
+
+// const getAccessToken = liff.getAccessToken()
+// console.log(`getAccessToken: ${getAccessToken}`)
+
+// const profile = liff.getProfile()
+// console.log(`profile: ${profile}`)
+
+// const profilePlus = liff.getProfilePlus()
+// console.log(`profilePlus: ${profilePlus}`)
